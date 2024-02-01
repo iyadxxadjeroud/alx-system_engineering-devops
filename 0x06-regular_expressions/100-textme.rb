5adb580fff53d7e2d3e46612a6ee5c04124807a1
iyadxxadjeroud <iyadadjeroud10@gmail.com>
2024-02-02 00:24:08 +0100

    DONE

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
+#The sender phone number or name and receiver name (including country code if present)
 puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
